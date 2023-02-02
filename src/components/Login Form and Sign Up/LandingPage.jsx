ed0e0ae6ef2d0e10d317fa2dd9c0c2ef66966b2c
RishabMandal <rishab829@gmail.com>
2023-02-02 13:00:41 +0530

    Donation page updated

--- src/components/Login Form and Sign Up/LandingPage.jsx
+++ src/components/Login Form and Sign Up/LandingPage.jsx
@@ -31,7 +31,7 @@ const LandingPage = () => {
           <NavLink className="bg-red-600 block text-white p-2 m-5" to="/login">
             Go to Login Page
           </NavLink>
-          <div>
+          {/* <div>
             <div className="min-h-screen">
               <section class="text-gray-600 body-font">
                 <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
@@ -58,6 +58,59 @@ const LandingPage = () => {
                 </div>
               </section>
             </div>
+          </div> */}
+          <div className="bg-gray-100 min-h-screen">
+            <header className="bg-white">
+              <nav className="flex items-center justify-between px-6 py-4">
+                <div className="text-xl font-bold text-gray-900">
+                  PharmaCare
+                </div>
+                <div className="flex">
+                  <a
+                    href="#"
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full"
+                  >
+                    Products
+                  </a>
+                  <a
+                    href="#"
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full ml-4"
+                  >
+                    About
+                  </a>
+                  <a
+                    href="#"
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full ml-4"
+                  >
+                    Contact
+                  </a>
+                </div>
+              </nav>
+            </header>
+            <section className="py-20">
+              <div className="container mx-auto">
+                <h1 className="text-5xl font-bold text-center text-gray-900">
+                  Welcome to PharmaCare
+                </h1>
+                <p className="text-xl text-center text-gray-600 mt-4">
+                  Your trusted source for all your pharmaceutical needs.
+                </p>
+                <div className="flex flex-col items-center mt-16">
+                  <a
+                    href="#"
+                    className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full"
+                  >
+                    Browse Products
+                  </a>
+                  <a
+                    href="#"
+                    className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full mt-4"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       )}
