3413d2f85cf78d2b1afc0054dc591feb98f64d77
Ishaan Mavinkurve <ishaanmavinkurve16@gmail.com>
2023-02-02 18:13:39 +0530

    contact us page

--- src/components/Logged In Contents/Contact.jsx
+++ src/components/Logged In Contents/Contact.jsx
@@ -17,89 +17,83 @@ const Contact = () => {
 
   return (
     <div>
-      <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            {/* <iframe
-              width="100%"
-              height="100%"
-              class="absolute inset-0"
-              frameborder="0"
-              title="map"
-              marginheight="0"
-              marginwidth="0"
-              scrolling="no"
-              src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-              style="filter: grayscale(1) contrast(1.2) opacity(0.4);"
-            ></iframe> */}
-            <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-              <div class="lg:w-1/2 px-6">
-                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                  ADDRESS
-                </h2>
-                <p class="mt-1">
-                  Photo booth tattooed prism, portland taiyaki hoodie neutra
-                  typewriter
-                </p>
-              </div>
-              <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                  EMAIL
-                </h2>
-                <a class="text-red-500 leading-relaxed">example@email.com</a>
-                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
-                  PHONE
-                </h2>
-                <p class="leading-relaxed">123-456-7890</p>
-              </div>
-            </div>
-          </div>
-          <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
-              Feedback
+      <section class="px-[20vw] bg-green-400 text-gray-600 body-font relative">
+      <h1 class="p-5 title-font text-6xl mb-4 font-medium text-white">
+                    Get in Touch!
+                  </h1>
+        <div class="text-left container px-5 py-24 mx-auto flex">
+          <div class="shadow-2xl p-5 border rounded-3xl bg-white flex flex-col w-[60vw]">
+            <h2 class="text-center text-4xl text-gray-900 mb-1 font-medium">
+              Contact Us
             </h2>
-            <p class="leading-relaxed mb-5 text-gray-600">
-              Post-ironic portland shabby chic echo park, banjo fashion axe
-            </p>
+            <div className="flex flex-row p-2.5">
+            <div className="p-5">
             <div class="relative mb-4">
-              <label for="name" class="leading-7 text-sm text-gray-600">
+              <label for="name" className="text-xl text-gray-600">
                 Name
-              </label>
+              </label><br/>
               <input
                 type="text"
                 id="name"
                 name="name"
-                class="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                class="w-[30vw] bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="John Doe"
               />
             </div>
+            <br/>
             <div class="relative mb-4">
-              <label for="email" class="leading-7 text-sm text-gray-600">
+              <label for="email" class="leading-7 text-xl text-gray-600">
                 Email
-              </label>
+              </label><br/>
               <input
                 type="email"
                 id="email"
                 name="email"
-                class="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                class="w-[30vw] bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="johndoe@example.com"
               />
             </div>
+            
             <div class="relative mb-4">
-              <label for="message" class="leading-7 text-sm text-gray-600">
-                Message
-              </label>
+              <br/>
+              <label for="email" class="leading-7 text-xl text-gray-600">
+                Phone Number
+              </label><br/>
+              <input
+                type="tel"
+                id="email"
+                name="email"
+                class="w-[30vw] bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="9999888770"
+              />
+            </div>
+            <p class="text-s text-gray-500 mt-3">
+            <input
+                  className="my-auto w-5 h-5 mx-3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+              Recieve newsletters and reular updates from our website
+            </p>
+            </div>
+            <div class="relative mb-4">
               <textarea
                 id="message"
                 name="message"
-                class="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                class="mt-5 w-full bg-gray-200 outline-none rounded h-[40vh] w-[20vw] focus:border-green-500 focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                placeholder="Write your message here..."
               ></textarea>
             </div>
-            <button class="text-white font-semibold bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-700 rounded text-lg">
+            </div>
+            
+            
+            
+            
+            <button class="ml-80 text-white w-1/3 font-semibold bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-700 rounded text-lg">
               Submit
             </button>
-            <p class="text-xs text-gray-500 mt-3">
-              Chicharrones blog helvetica normcore iceland tousled brook viral
-              artisan.
-            </p>
+            
           </div>
         </div>
       </section>
