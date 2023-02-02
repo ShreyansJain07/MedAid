5e20198edb35f2f969b16e1a318de686c5cb14e9
Krish Manghani <krishmanghani@gmail.com>
2023-02-02 16:02:36 +0530

    hello

--- server/medicine.js
+++ server/medicine.js
@@ -31,51 +31,14 @@ var authSchema = new mongoose.Schema({
 
 //Model
 var Log = mongoose.model("Log", authSchema);
-// 'https://serpapi.com/search.json?q=dolo+650+details+1mg&location=Maharashtra,+India&hl=hi&gl=in&google_domain=google.co.in&key=5d1b79b4ee809fa8365d09f2d36a866dda3b7e1e2f93ad1c59726eff1454f4f2'
-// let url = 'q=&location=Maharashtra,+India&hl=hi&gl=in&google_domain=google.co.in&key='
-// const base = 'https://serpapi.com/search.json?'
-// let query = 'dolo+650+details+1mg'
-// const apikey = '5d1b79b4ee809fa8365d09f2d36a866dda3b7e1e2f93ad1c59726eff1454f4f2'
 
 app.get("/medicine", (req, res)=>{
     console.log('yoo');
     requestify.get('https://serpapi.com/search.json?q=dolo+650+details+1mg&location=Maharashtra,+India&hl=hi&gl=in&google_domain=google.co.in&key=5d1b79b4ee809fa8365d09f2d36a866dda3b7e1e2f93ad1c59726eff1454f4f2')
     .then(function(response) {
-        // response.getBody();
         console.log(response.getBody().organic_results[0]);
     }
     );
 })
-
-
-
-// app.post("/signup", (req, res) => {
-//   console.log(req.body);
-//   var myData = new Log(req.body);
-//   myData
-//     .save()
-//     .then(() => {
-//       console.log("done");
-//       res.send("This data has been saved to the database");
-//     })
-//     .catch(() => {
-//       console.log("not done");
-//       res.status(400).send("Item was not saved to the database");
-//     });
-// });
-
-// app.post("/login", (req, res) => {
-//   var username = req.body.username;
-//   var password = req.body.password;
-
-//   Log.find({username: `${username}`}, {password: `${password}`}, (err, docs)=>{
-//     if(docs.length == 0){
-//         res.status(400).send('Invalid credentials')
-//     }
-//     else{
-//         res.send("true");
-//     }
-//   });  
-// })
 
 app.listen(port, () => console.log("Connected to port " + port));
