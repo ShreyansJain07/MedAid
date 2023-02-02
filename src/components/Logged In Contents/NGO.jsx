2ec65ba6d6edb2a667e38396fbad9c1f222c4e1c
Shreyans jain <shreyansjain1603@gmail.com>
2023-02-02 16:33:32 +0530

    ngo

--- src/components/Logged In Contents/NGO.jsx
+++ src/components/Logged In Contents/NGO.jsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 
 const NGO = () => {
+
+  const [current,setCurrent] = useState("NGO 1")
   return (
     <div className="bg-gray-200 p-5">
       <div className="bg-white flex rounded-xl flex-row">
         <div className="min-h-[60vh] w-[40vw] py-5 px-4">
           <div className="text-2xl font-bold text-green-600 mb-5">Groups</div>
-          <div className="cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
+          <div onClick={()=> setCurrent("NGO 1")} className="cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
             NGO 1
           </div>
-          <div className="my-4 cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
+          <div onClick={()=> setCurrent("NGO 2")} className="my-4 cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
             NGO 2
           </div>
-          <div className="my-4 cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
+          <div onClick={()=> setCurrent("NGO 3")} className="my-4 cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
             NGO 3
           </div>
-          <div className="my-4 cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
+          <div onClick={()=> setCurrent("NGO 4")} className="my-4 cursor-pointer w-full transition ease-in hover:bg-green-400 font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
             NGO 4
           </div>
         </div>
         <div className=" border-l-2 min-h-[60vh] w-full p-5">
           <div className="w-full font-semibold rounded-lg border-2 border-green-600 py-2 px-6">
-            NGO 1
+            {current}
           </div>
           <div className="flex flex-row my-5">
             <div className="cursor-pointer text-sm font-semibold hover:bg-green-600 w-full bg-green-500 text-white py-2 rounded-md mx-1">
