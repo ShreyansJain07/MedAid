f3cafd0955313782a518e44eeb4efb7ddcb2424e
Krish Manghani <krishmanghani@gmail.com>
2023-02-02 12:56:10 +0530

    meds api

--- src/components/Logged In Contents/Contact.jsx
+++ src/components/Logged In Contents/Contact.jsx
@@ -3,13 +3,15 @@ import axios from "axios";
 const Contact = () => {
 
   useEffect(()=>{
-  axios.get("http://serpapi.com/search.json?q=dolo+650+details+1mg&location=Maharashtra,+India&hl=hi&gl=in&google_domain=google.co.in&key=5d1b79b4ee809fa8365d09f2d36a866dda3b7e1e2f93ad1c59726eff1454f4f2")
-  .then(function(response){
+  axios
+    // .get(`http://localhost:5000/medicine?query=${groupname}`)
+    .get(`http://localhost:5000/medicine?query=dolo+650`)
+    .then(function(response){
     console.log(response.data);
-  })
-  .catch(function(error){
+    })
+    .catch(function(error){
     console.log(error);
-  })
+    })
 },[])
 
   return (
